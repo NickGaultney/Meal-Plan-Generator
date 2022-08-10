4565430c65f0eff20c31400c4f92d28fd9f3ee34
ACombs93 <austen.combs@gmail.com>
2022-08-10 16:20:19 -0700

    update ingredient move error

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,6 @@ class RecipesController < ApplicationController
   # GET /recipes or /recipes.json
   def index
     @recipes = Recipe.all
-    @ingredients = Ingredient.all
   end
 
   # GET /recipes/1 or /recipes/1.json
@@ -14,6 +13,7 @@ class RecipesController < ApplicationController
   # GET /recipes/new
   def new
     @recipe = Recipe.new
+    @ingredients = Ingredient.all
   end
 
   # GET /recipes/1/edit
